3d64337c154242acffa48ebfba6bf2dc947366a5
Mario <mniepytalski@gmail.com>
2021-06-29 16:16:18 +0200

    add watchdog logic

--- balcony_irrigation/irrygation/irrygation.ino
+++ balcony_irrigation/irrygation/irrygation.ino
@@ -1,7 +1,6 @@
 #include "RTCDS1307.h"
 #include <stdio.h>
 #include <avr/wdt.h>
-//#include <String.h>
 #include <Wire.h>
 #include <hd44780.h>                       // main hd44780 header
 #include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
@@ -11,6 +10,7 @@
 // LCD geometry
 const int LCD_COLS = 20;
 const int LCD_ROWS = 4;
+
 
 RTCDS1307 rtc(0x68);
 
@@ -83,7 +83,7 @@ class Clock {
 ////////////////////////////////////////////////////////////////////////
 
 hd44780_I2Cexp lcd; // declare lcd object: auto locate & auto config expander chip  
-int status;
+int lcdStatus = 1; // 1 -on, 0-off
 
 
 Clock clock;
@@ -93,78 +93,143 @@ int pumpPin2 = 6;     // pump 2
 
 int key1 = 5;
 int key2 = 4;
+int key3 = 8;
 
-boolean pumpStatus = false;
+int engineCounter1 = 0;
+int engineCounter2 = 0;
+int timerCounter = 0;
+int engineWorkTime = 5;
 
 void setup()
 {
-  status = lcd.begin(LCD_COLS, LCD_ROWS);
-  if(status) {
-    hd44780::fatalError(status); // does not return
-  }  
-  Serial.begin(115200);
+  if (lcdStatus==1 ) {
+    lcdStatus = lcd.begin(LCD_COLS, LCD_ROWS);
+    if(lcdStatus) {
+      hd44780::fatalError(lcdStatus); // does not return
+    }
+  }
 
   pinMode(pumpPin1, OUTPUT);
   pinMode(pumpPin2, OUTPUT);
-  pumpsOff();
+  pumpOff(pumpPin1);
+  pumpOff(pumpPin2);
   
   pinMode(key1, OUTPUT);
   pinMode(key2, INPUT);
+  pinMode(key3, INPUT);
   digitalWrite(key1, LOW);
   digitalWrite(key2, HIGH);
+  digitalWrite(key3, HIGH);
   
   clock.start();
 //  clock.setTime();
+
+  initWatchdog();
 }
 
 void loop()
 {
   clock.getTime();
-//  clock.sendToSerial();
 
-  lcd.setCursor(0, 0);
-  lcd.print("System podlewania");
-
+  if (lcdStatus!=1 ) {
+    lcd.setCursor(3, 0);
+    lcd.print("System podlewania");
+  }
   for ( ; ; ) {
     wdt_reset();
-    lcd.setCursor(0, 1);
     clock.getTime();
-    lcd.print(clock.getFullTimeToString());
- 
-    if(digitalRead(key2) == LOW ) {
-      pumpsOn();
-    } else {
-      if ( checkEnginesStatus() ) {
-        pumpsOn();
-      } else {
-        pumpsOff();
-      }
+    if ( lcdStatus!=1 ) {
+      lcd.setCursor(0, 1);
+      lcd.print(clock.getFullTimeToString());
     }
+
+    checkKeysStatus();
+    if(digitalRead(key2) == LOW ) {
+      pumpsOn(1);
+    }
+    if(digitalRead(key3) == LOW ) {
+      pumpsOn(2);
+    }
+    if ( checkEnginesStatus(9,0) ) {
+      pumpsOn(1);
+    }
+    if ( checkEnginesStatus(9,5) ) {
+      pumpsOn(2);          
+    }
+    
+    checkPumps();
+    if ( timerCounter>0 ) {
+      timerCounter--;
+    } 
     delay(1000);
   }
 }
 
-boolean checkEnginesStatus() {
-  return clock.getHour()== 20 && clock.getMinute() ==15 ;
+void checkKeysStatus() {
+    if (lcdStatus!=1 ) {
+      lcd.setCursor(0, 0);
+      lcd.print(digitalRead(key2));
+      lcd.setCursor(1, 0);
+      lcd.print(digitalRead(key3));
+    }
 }
 
-
-void pumpsOn() {
-    lcd.setCursor(0, 2);
-    lcd.print("ON ");
-    digitalWrite(pumpPin1, LOW);
-    digitalWrite(pumpPin2, LOW);
-    pumpStatus = true;
+boolean checkEnginesStatus(int hour,int minute) {
+  if ( timerCounter>0 ) {
+    return false;
+  } 
+//  boolean test = clock.getHour() == hour && clock.getMinute() == minute ;
+  boolean test = clock.getMinute()%10 == minute ;
+  if ( test ) {
+    timerCounter=60; // block it on X seconds
+  }
+  return test;
 }
 
-void pumpsOff() {
-    lcd.setCursor(0, 2);
-    digitalWrite(pumpPin1, HIGH);
-    digitalWrite(pumpPin2, HIGH);
-    lcd.print("OFF");
-    pumpStatus = false;
+void pumpsOn(int number) {
+    if ( number==1 ) {
+      pumpOnIfPossible(&engineCounter1,pumpPin1);
+    } 
+    if ( number==2 ) {
+      pumpOnIfPossible(&engineCounter2,pumpPin2);
+    }
 }
 
+void pumpOnIfPossible(int *engineCounter,int pumpPin) {
+  if ( *engineCounter<=0 ) {
+    digitalWrite(pumpPin, LOW);
+    *engineCounter = engineWorkTime;
+  }
+}
+
+void checkPumps() {
+  checkPumpAndOff(&engineCounter1,pumpPin1);
+  checkPumpAndOff(&engineCounter2,pumpPin2);
+  printInfo(2, pumpPin1,engineCounter1);
+  printInfo(3, pumpPin2,engineCounter2);
+}
+
+void printInfo(int lcdRow, int pumpPin, int engineCounter) {
+  if ( lcdStatus==1 ) {
+    return;
+  }
+  char info[20];
+  lcd.setCursor(0, lcdRow);
+  sprintf(info,"%d>%3d, timer:%2d",pumpPin, engineCounter, timerCounter);
+  lcd.print(info);  
+}
+
+void checkPumpAndOff(int *engineCounter,int pumpPin) {
+    if ( *engineCounter>0 ) {
+      *engineCounter = *engineCounter - 1 ;
+    } else {
+      pumpOff(pumpPin);
+    }
+}
+
+void pumpOff(int pumpNumber) {
+  digitalWrite(pumpNumber, HIGH);
+}
 
 void initWatchdog() {
   cli();         // disable all interrupts
