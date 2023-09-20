9f0dcf50fb53aef4a9a93a40827a2f9cf577f22d
mariuszniepytalski <mniepytalski@gmail.com>
2023-09-20 15:47:59 +0200

    Separate display logic

--- balcony_irrigation/irrygation/irrygation.ino
+++ balcony_irrigation/irrygation/irrygation.ino
@@ -1,51 +1,11 @@
 #include "RTCDS1307.h"
 #include "LowPower.h"
 #include <stdio.h>
-#include <Wire.h>
-#include <hd44780.h>                       // main hd44780 header
-#include <hd44780ioClass/hd44780_I2Cexp.h> // i2c expander i/o class header
-#include "key.h"
 #include "pump.h"
 #include "clock.h"
 #include "watchdog.h"
-
-/*-----( Declare Constants )-----*/
-// LCD geometry
-const int LCD_COLS = 20;
-const int LCD_ROWS = 4;
-
-////////////////////////////////////////////////////////////////////////
-hd44780_I2Cexp lcd; // declare lcd object: auto locate & auto config expander chip  
-int lcdStatus = 1; // 1 -on, 0-off
-
-class Display {
-private:
-    int pin;
-
-public:
-    explicit Display();
-    void init();
-    void print(int col, int row, int digit);
-};
-
-Display::Display() {
-}
-
-void Display::init() {
-  if (lcdStatus==1 ) {
-    lcdStatus = lcd.begin(LCD_COLS, LCD_ROWS);
-    if(lcdStatus) {
-      hd44780::fatalError(lcdStatus); // does not return
-    }
-  }  
-}
-
-void Display::print(int col, int row, int digit) {
-  if (lcdStatus!=1 ) {
-    lcd.setCursor(col, row);  
-    lcd.print(digit);
-  }
-}
+#include "display.h"
+#include "key.h"
 
 Watchdog watchdog;
 Clock clock;
@@ -78,25 +38,18 @@ void loop()
   clock.getTime();
 
   for ( ; ; ) {
-    if (lcdStatus!=1 ) {
-      lcd.setCursor(3, 0);
-      sprintf(buffer,"System v07 %6x", systemCounter);
-      lcd.print(buffer);
-    }
+    sprintf(buffer,"System v08 %6x", systemCounter);
+    display.print(3, 0, buffer);
 
     watchdog.reset();
     clock.getTime();
-
-    if ( lcdStatus!=1 ) {
-      lcd.setCursor(0, 1);
-      lcd.print(clock.getFullTimeToString());
-    }
+    display.print(0, 1, clock.getFullTimeToString());
 
     printKeysStatus();
-    if(checkEnginesStatus(8,0) || key1.isKeyPressed()) {
+    if(checkConditionToOnEngines(8,0) || key1.isKeyPressed()) {
       pump1.on();
     }
-    if(checkEnginesStatus(8,5) || key2.isKeyPressed()) {
+    if(checkConditionToOnEngines(8,5) || key2.isKeyPressed()) {
       pump2.on();
     }
     
@@ -106,14 +59,18 @@ void loop()
       timerCounter--;
     } 
 
+    sleep1s();
+    systemCounter++;
+  }
+}
+
+void sleep1s() {
     if ( systemCounter>=0 && systemCounter<(2*60) ) {
       delay(1000);
     } else {
       LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
       LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
-    }
-    systemCounter++;
-  }
+    }  
 }
 
 void printKeysStatus() {
@@ -121,7 +78,7 @@ void printKeysStatus() {
   display.print(1, 0, key2.read());   
 }
 
-boolean checkEnginesStatus(int hour,int minute) {
+boolean checkConditionToOnEngines(int hour,int minute) {
   if ( timerCounter>0 ) {
     return false;
   } 
@@ -143,10 +100,6 @@ void printInfo() {
 }
 
 void printInfo(int lcdRow, int workCounter, int engineCounter) {
-  if ( lcdStatus==1 ) {
-    return;
-  }
-  lcd.setCursor(0, lcdRow);
   sprintf(buffer,"%2X>%3d, timer:%2d", workCounter, engineCounter, timerCounter);
-  lcd.print(buffer);  
+  display.print(0, lcdRow, buffer);  
 }
